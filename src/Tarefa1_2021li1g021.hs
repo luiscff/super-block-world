aaa792d718663973961c3c3452330170bfbb3969
a100715 <a100715@alunos.uminho.pt>
2021-11-11 09:33:37 +0000

    2. Declarar a Existência de uma Porta

--- src/Tarefa1_2021li1g021.hs
+++ src/Tarefa1_2021li1g021.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 {- |
 Module      : Tarefa1_2021li1g021
 Description : Validação de um potencial mapa
@@ -9,7 +10,17 @@ Módulo para a realização da Tarefa 1 do projeto de LI1 em 2021/22.
 module Tarefa1_2021li1g021 where
 
 import LI12122
+import Data.Vector.Primitive (elemIndices)
+
 
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
-validaPotencialMapa pecas = undefined
+validaPotencialMapa [] = False
+validaPotencialMapa ((p,c):t)
+    | contador (Porta,(0,0)) ((p,c):t) == 1 = True
+    | otherwise = False
 
+contador :: (Peca, Coordenadas) -> [(Peca, Coordenadas)] -> Int
+contador _ [] = 0
+contador (a,_) (x:xs)
+    | a == fst x = 1 + contador (a,(0,0)) xs 
+    | otherwise = contador (a,(0,0)) xs 
