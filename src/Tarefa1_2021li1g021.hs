bfa3b57b39fea743b8535d8f0c32d11177dd617a
a100715 <a100715@alunos.uminho.pt>
2021-11-18 09:13:58 +0000

    Tarefa 1 - Step 2 Concluido

--- src/Tarefa1_2021li1g021.hs
+++ src/Tarefa1_2021li1g021.hs
@@ -1,4 +1,6 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+
 {- |
 Module      : Tarefa1_2021li1g021
 Description : Validação de um potencial mapa
@@ -7,23 +9,28 @@ Copyright   : Flávio David Rodrigues Sousa <a100715@alunos.uminho.pt>;
 
 Módulo para a realização da Tarefa 1 do projeto de LI1 em 2021/22.
 -}
+
 module Tarefa1_2021li1g021 where
 
 import LI12122
 
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
 validaPotencialMapa [] = False
-validaPotencialMapa ((p,c):t)   --esta parte verifica se só há uma porta e chama uma auxiliar
-    | contador (Porta,(0,0)) ((p,c):t) == 1 = auxDeclPeca ((p,c):t)
-    | otherwise = False
+validaPotencialMapa ((p, c) : t) --esta parte verifica se só há uma porta e chama uma auxiliar
+  | contador (Porta, (0, 0)) ((p, c) : t) == 1 = norepete ((p, c) : t)
+  | otherwise = False
 
 contador :: (Peca, Coordenadas) -> [(Peca, Coordenadas)] -> Int
 contador _ [] = 0
-contador (a,_) (x:xs)
-    | a == fst x = 1 + contador (a,(0,0)) xs 
-    | otherwise = contador (a,(0,0)) xs 
+contador (a, _) (x : xs)
+  | a == fst x = 1 + contador (a, (0, 0)) xs
+  | otherwise = contador (a, (0, 0)) xs
 
+norepete :: [(Peca,Coordenadas)] -> Bool
+norepete [] = False
+norepete ((_,c):t) = thereis c t
 
-auxDeclPeca :: [(Peca, Coordenadas)] ->
---(INCOMPLETO) esta parte verifica se há mais do que uma declaração de peça para a mesma posição
+thereis :: Coordenadas -> [(Peca, Coordenadas)] -> Bool
+thereis c1 [] = True
+thereis c1 ((p,c):t) = c1 /= c && thereis c1 t
 
