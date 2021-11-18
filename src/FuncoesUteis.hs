0caa95e731e67374d964a6eca1a5287b26cdb506
Luís <a100549@alunos.uminho.pt>
2021-11-18 18:39:49 +0000

    adição da função constroiMapa(incompleta) e alteração de outras em FuncoesUteis.hs

--- src/FuncoesUteis.hs
+++ src/FuncoesUteis.hs
@@ -2,27 +2,31 @@ module FuncoesUteis where
 import LI12122
 -- Falta colocar o cabeçalho e as legendas
 maiorCoordenada :: [(Peca,Coordenadas)] -> Coordenadas
-maiorCoordenada l = maiorCoordenadaAUX l (0,0) --esta função dá a maior coordenada de uma lista
+maiorCoordenada l = maiorCoordenadaAUX l (0,0) --dá a maior coordenada de uma lista
 
 maiorCoordenadaAUX :: [(Peca,Coordenadas)] -> Coordenadas -> Coordenadas
 maiorCoordenadaAUX [] (a,b) = (a,b)
-maiorCoordenadaAUX ((p,(x,y)):t) (a,b) --com a maior coordenada sabemos a dimensão da lista
+maiorCoordenadaAUX ((p,(x,y)):t) (a,b) 
     |x > a && y > b = maiorCoordenadaAUX t (x,y)
     |x > a = maiorCoordenadaAUX t (x,b)
     |y > b = maiorCoordenadaAUX t (a,y)
     |otherwise = maiorCoordenadaAUX t (a,b)
 
-menorElem :: Ord a => [a] -> a
-menorElem [] = error "não existe menorElem de uma lista vazia"
-menorElem [x] = x
-menorElem (x:xs)
-    |x >= head xs = menorElem xs
-    |otherwise = menorElem (x: (tail xs))
+menorElemY :: [(Peca,Coordenadas)] -> (Peca,Coordenadas) --retorna um elemento com o menor Y
+menorElemY [] = error "não existe menorElemY de uma lista vazia"
+menorElemY [x] = x
+menorElemY ((p,(x,y)):t)
+    |y >= snd(maiorCoordenada [head t]) = menorElemY t
+    |otherwise = menorElemY ( [(p,(x,y))] ++ (tail t))
 
+maiorElemY :: [(Peca,Coordenadas)] -> (Peca,Coordenadas) --retorna um elemento com o maior Y
+maiorElemY [] = error "não existe maiorElemY de uma lista vazia"
+maiorElemY [x] = x
+maiorElemY ((p,(x,y)):t)
+    |y <= snd(maiorCoordenada [head t]) = maiorElemY t
+    |otherwise = maiorElemY ( [(p,(x,y))] ++ (tail t))
 
-maiorElem :: Ord a => [a] -> a
-maiorElem [] = error "não existe maiorElem de uma lista vazia"
-maiorElem [x] = x
-maiorElem (x:xs)
-    |x <= head xs = maiorElem xs           
-    |otherwise = maiorElem (x: (tail xs))
+removerElem _ [] = [] --remove um elemento de uma lista
+removerElem x (y:ys) 
+    | x == y    = removerElem x ys
+    | otherwise = y : removerElem x ys
--- src/Tarefa2_2021li1g021.hs
+++ src/Tarefa2_2021li1g021.hs
@@ -7,11 +7,43 @@ Copyright   : Flávio David Rodrigues Sousa <a100715@alunos.uminho.pt>;
 Módulo para a realização da Tarefa 2 do projeto de LI1 em 2021/22.
 -}
 module Tarefa2_2021li1g021 where
-
+import FuncoesUteis
 import LI12122
 
+exemplo1 = [(Porta, (0, 2)),(Bloco, (0, 3)), (Bloco, (4,4)), (Bloco, (1, 3)), (Bloco, (2, 3))]
+
+--para já só constrói um mapa com 1 x (x=0), tudo o resto dá errado (os elementos também só são organizados pelo y da coordenada)
 constroiMapa :: [(Peca, Coordenadas)] -> Mapa
-constroiMapa pecas = undefined
+constroiMapa [] = [[]]
+constroiMapa l = constroiMapaAUX (ordenaY l) (criarMapaVazio (maiorCoordenada l)) 0 --vai dar match das coordenadas dadas com as da função criarMapaVazio e meter lá as peças dadas
+--l tem que ser ordenada
+constroiMapaAUX [] _ _ = []
+constroiMapaAUX ((p,(x,y)):t) (l:ls) n
+    | y == n = [[p]] ++ constroiMapaAUX t ls (n+1) -- vai trabalhar e modificar l para q a lista de listas (antigamente vazia) comece a ter elementos (nas posiçoes corretas)
+    | y /= n = [l] ++ constroiMapaAUX ((p,(x,y)):t) ls (n+1)
+
+ordenaY :: [(Peca, Coordenadas)] -> [(Peca, Coordenadas)]
+ordenaY l = auxOrdenaY l [] --ordena uma lista relativamente ao y da coordenada 
+
+auxOrdenaY [] l = l
+auxOrdenaY l1 l2 = auxOrdenaY(removerElem (menorElemY l1) l1) (l2 ++ [menorElemY l1])
+
+--criarMapaVazio :: ------- -> Mapa
+criarMapaVazio (x,y) = criarMapaVazioAUX1 (x,y) (y+1) [] --esta função pega numa coordenada e cria um mapa vazio com essas dimensões
+
+               --vai pegar nas listas dadas pela função criarMapaVazioAUX2 e criar uma lista dessas listas
+criarMapaVazioAUX1 (x,y) 0 l = []
+criarMapaVazioAUX1 (x,y) n l = criarMapaVazioAUX2 (x,y) (0,0) [] : criarMapaVazioAUX1 (x,y) (n-1) l 
+
+--criarMapaVazioAUX2 :: Coordenadas -> Coordenadas -> [a]                  ERRO
+--vai dar uma linha do mapa de cada vez
+criarMapaVazioAUX2 (x,y) (a,b) l
+    |a < x = criarMapaVazioAUX2 (x,y) (a+1,b) (l ++ [Vazio])
+    |a == x = l ++ [Vazio]
+
+coordenadaUltimoTuplo((p,c):t)  --dá a última coordenada do último tuplo
+    |t == [] = c          --nao me lembro para que ia servir esta funçao mas pode ser útil no futuro
+    |otherwise = coordenadaUltimoTuplo t
 
 desconstroiMapa :: Mapa -> [(Peca, Coordenadas)]
 desconstroiMapa mapa = undefined
