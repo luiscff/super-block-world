ab64388c88037cae74f19018286fecfcf81af973
a100715 <a100715@alunos.uminho.pt>
2021-11-18 11:42:39 +0000

    Tarefa 1 - Step 3 Concluido

--- src/Tarefa1_2021li1g021.hs
+++ src/Tarefa1_2021li1g021.hs
@@ -9,8 +9,8 @@ Módulo para a realização da Tarefa 1 do projeto de LI1 em 2021/22.
 
 module Tarefa1_2021li1g021 where
 
-import LI12122
 import FuncoesUteis
+import LI12122
 
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
 validaPotencialMapa [] = False
@@ -24,19 +24,19 @@ contador (a, _) (x : xs)
   | a == fst x = 1 + contador (a, (0, 0)) xs
   | otherwise = contador (a, (0, 0)) xs
 
-norepete :: [(Peca,Coordenadas)] -> Bool
-norepete [] = True
-norepete ((p,c):t) = (thereisnt c t && norepete t) && emptyspace ((p,c):t)
-    where thereisnt :: Coordenadas -> [(Peca, Coordenadas)] -> Bool
-          thereisnt c1 [] = True
-          thereisnt c1 ((p,c):t) = c1 /= c && thereisnt c1 t
-
+norepete :: [(Peca, Coordenadas)] -> Bool
+norepete [] = True -- Ver quando tenho 2 objetos com a mesma coordenada deve dar True
+norepete ((p, c) : t) = (thereisnt c t && norepete t) && emptyspace ((p, c) : t)
+  where
+    thereisnt :: Coordenadas -> [(Peca, Coordenadas)] -> Bool
+    thereisnt c1 [] = True
+    thereisnt c1 ((p, c) : t) = c1 /= c && thereisnt c1 t
 
 emptyspace :: [(Peca, Coordenadas)] -> Bool
 emptyspace [] = False
-emptyspace ((p,c):t)
-    | p == Vazio || emptyspace t || tamanho (maiorCoordenada ((p,c):t)) > length ((p,c):t) = True
-    | otherwise = False
-    where tamanho :: (Int,Int) -> Int
-          tamanho (x,y) = (x+1)*(y+1)
-
+emptyspace ((p, c) : t)
+  | p == Vazio || emptyspace t || tamanho (maiorCoordenada ((p, c) : t)) > length ((p, c) : t) = True
+  | otherwise = False
+  where
+    tamanho :: (Int, Int) -> Int
+    tamanho (x, y) = (x + 1) * (y + 1)
