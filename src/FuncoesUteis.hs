b8a2d8f95045067b66cbb258a691173850275623
Luís Figueiredo <a100549@alunos.uminho.pt>
2021-11-18 15:18:00 +0000

    Adicionei funções úteis

--- src/FuncoesUteis.hs
+++ src/FuncoesUteis.hs
@@ -11,3 +11,18 @@ maiorCoordenadaAUX ((p,(x,y)):t) (a,b) --com a maior coordenada sabemos a dimens
     |x > a = maiorCoordenadaAUX t (x,b)
     |y > b = maiorCoordenadaAUX t (a,y)
     |otherwise = maiorCoordenadaAUX t (a,b)
+
+menorElem :: Ord a => [a] -> a
+menorElem [] = error "não existe menorElem de uma lista vazia"
+menorElem [x] = x
+menorElem (x:xs)
+    |x >= head xs = menorElem xs
+    |otherwise = menorElem (x: (tail xs))
+
+
+maiorElem :: Ord a => [a] -> a
+maiorElem [] = error "não existe maiorElem de uma lista vazia"
+maiorElem [x] = x
+maiorElem (x:xs)
+    |x <= head xs = maiorElem xs           
+    |otherwise = maiorElem (x: (tail xs))
