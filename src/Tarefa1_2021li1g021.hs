97dd99d23db445bbb27c38986b8dcda7af5aa5ba
a100715 <a100715@alunos.uminho.pt>
2021-11-20 17:40:03 +0000

    (Incompleto)

--- src/Tarefa1_2021li1g021.hs
+++ src/Tarefa1_2021li1g021.hs
@@ -40,3 +40,48 @@ emptyspace ((p, c) : t)
   where
     tamanho :: (Int, Int) -> Int
     tamanho (x, y) = (x + 1) * (y + 1)
+
+caixas :: [(Peca, Coordenadas)] -> [(Peca, Coordenadas)]               -- Dá uma lista com apenas as Caixas
+caixas [] =[]
+caixas ((p,c):t)
+  | p == Caixa = (p,c) : caixas t
+  | otherwise = caixas t
+
+removecaixaseporta :: [(Peca, Coordenadas)] -> [(Peca, Coordenadas)]    -- Dá uma lista sem as caixas e sem a porta
+removecaixaseporta [] = []
+removecaixaseporta ((p,c):t)
+  | p == Caixa = removecaixaseporta t
+  | p == Porta = removecaixaseporta t
+  | otherwise = (p,c) : removecaixaseporta t
+
+
+checkcaixas :: [(Peca, Coordenadas)] -> Bool
+checkcaixas [] = True
+checkcaixas l
+  | x == x1 && y == y1+1 && checkcaixasaux t t1 = True
+  | x == x1 && y /= y1+1 = checkcaixasaux [(p,(x,y))] t1
+  | x /= x1 = checkcaixasaux [(p,(x,y))] t1
+  | otherwise = False
+  where (p,(x,y)):t = caixas l
+        (p1,(x1,y1)):t1 = removecaixaseporta l
+
+checkcaixasaux :: [(Peca, Coordenadas)] -> [(Peca, Coordenadas)] -> Bool
+checkcaixasaux _ [] = False
+checkcaixasaux a b
+  | x == x1 && y == y1+1 = True
+  | x /= x1 = checkcaixasaux a t
+  | otherwise = False
+  where [(p,(x,y))] = a
+        (p1,(x1,y1)):t = b
+
+
+mapabase :: [(Peca, Coordenadas)] -> Bool
+mapabase [] = False
+mapabase l = mapabaseaux [menorElemY l] (outroselementos l)
+             where outroselementos :: [(Peca,Coordenadas)] -> [(Peca, Coordenadas)]
+                   outroselementos [] = []
+                   outroselementos ((p,c):t) = if (p,c) == menorElemY ((p,c):t) then t else (p,c) : outroselementos t
+
+mapabaseaux :: [(Peca,Coordenadas)] -> [(Peca, Coordenadas)] -> Bool
+mapabaseaux (p,(x,y)) (p1,(x1,y1):t)                                        -- Atenção que o menor elementos pode não ser um Bloco (mas terá de ser um bloco obrigatoriamente)
+  | p1 == Bloco && x==x1 && (y1 == y+1 || 
