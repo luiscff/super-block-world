3788444f32e0c11a2e3002cf64fa16cd93f8ec96
a100715 <a100715@alunos.uminho.pt>
2021-12-09 08:18:42 +0000

    Criação das Tarefas

---
+++ src/Tarefa5_2021li1g021.hs
@@ -0,0 +1,9 @@
+{- |
+Module      : Tarefa5_2021li1gXXX
+Description : Aplicação Gráfica
+
+Módulo para a realização da Tarefa 5 do projeto de LI1 em 2021/22.
+-}
+module Main where
+
+main = undefined
