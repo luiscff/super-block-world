1708bbfd66f935b62bb3cb827334e2518802d5ad
Luís <a100549@alunos.uminho.pt>
2022-01-04 14:39:27 +0000

    Início da Tarefa 5

--- src/Tarefa5_2021li1g021.hs
+++ src/Tarefa5_2021li1g021.hs
@@ -6,19 +6,62 @@ Módulo para a realização da Tarefa 5 do projeto de LI1 em 2021/22.
 -}
 module Main where
 import LI12122
-import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
+import Maps
+import FuncoesUteis
+import System.Exit
 
-menuInicial = undefined
+type Imagens = [Picture]
+
+data Menu = MP OpcoesIn | DentroJogo 
+
+instance Eq Menu where
+  (==) (MP Jogar) (MP Jogar) = True
+  (==) (MP Sair) (MP Sair) = True
+  (==) (DentroJogo) (DentroJogo) = True
+  (==) _ _ = False
+
+data OpcoesIn = Jogar | Sair
+
+type Estado = (Imagens , Menu , Jogo)
+
+desenhaEstado :: Estado -> IO Picture
+desenhaEstado e@(imagens, menu, jogo)
+    |menu == MP Jogar = return $ imagens !! 5
+    |menu == MP Sair = return $ imagens !! 6
+    |menu == DentroJogo = return $ desenhaJogo jogo
 
 desenhaJogo :: Jogo -> Picture
 desenhaJogo = undefined
 
-inputManager :: Event -> Jogo -> Jogo
-inputManager = undefined
+inputManager :: Event -> Estado -> IO Estado
+inputManager (EventKey (SpecialKey KeyDown) Down _ _ ) (i, MP Jogar, j) = return (i, MP Sair, j)
+inputManager (EventKey (SpecialKey KeyUp) Down _ _ ) (i, MP Jogar, j) = return (i, MP Sair, j)
+inputManager (EventKey (SpecialKey KeyDown) Down _ _ ) (i, MP Sair, j) = return (i, MP Jogar, j)
+inputManager (EventKey (SpecialKey KeyUp) Down _ _ ) (i, MP Sair, j) = return (i, MP Jogar, j)
+inputManager (EventKey (SpecialKey KeyEnter) Down _ _ ) (i, MP Sair, j) = exitSuccess
+inputManager (EventKey (SpecialKey KeyEnter) Down _ _ ) (i, MP Jogar, j) = return (i, DentroJogo, j)
+inputManager _ e = return e
 
-time :: Float -> Jogo -> Jogo
-time = undefined
+time :: Float -> Estado -> IO Estado
+time _ e = return e
 
 main :: IO ()
 main = do
-    play FullScreen black 20 menuInicial desenhaJogo inputManager time
+    bloco <- loadBMP "Imagens/bloco.bmp"
+    caixa <- loadBMP "Imagens/caixa.bmp"
+    castelo <- loadBMP "Imagens/castelo.bmp"
+    mariodireita <- loadBMP "Imagens/mariodireita.bmp"
+    marioesquerda <- loadBMP "Imagens/marioesquerda.bmp"
+    mpJogar <- loadBMP "Imagens/menu.bmp"
+    mpSair <- loadBMP ""
+    let estadoInicial = ([bloco, caixa, castelo, mariodireita, marioesquerda, mpJogar, mpSair], MP Jogar, m1e1)
+
+    playIO FullScreen 
+        black 
+        30 
+        estadoInicial 
+        desenhaEstado 
+        inputManager 
+        time
