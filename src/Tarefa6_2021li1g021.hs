89e4b7c5931abaa8c961d2db5177506873222894
a100715 <a100715@alunos.uminho.pt>
2022-01-08 21:31:10 +0000

    Finalização da Tarefa 6

--- src/Tarefa6_2021li1g021.hs
+++ src/Tarefa6_2021li1g021.hs
@@ -7,83 +7,120 @@ Módulo para a realização da Tarefa 6 do projeto de LI1 em 2021/22.
 module Tarefa6_2021li1gXXX where
 
 import LI12122
-import Tarefa2_2021li1g021
-
---resolveJogo :: Int -> Jogo -> Maybe [Movimento]
---resolveJogo 0 jogo = undefined --testar só se é possível acabar
---resolveJogo i jogo = undefined
-
-{-
-resolveJogo :: Jogo -> Maybe [Movimento]
-resolveJogo (Jogo ((x:xs):ys) (Jogador (c1,c2) direc caixa))
-    | direc == Este && caixa == False = resolveJogoaux (Jogo ((x:xs):ys)) (Jogador (c1,c2) Este False)
-    | direc == Este && caixa == True = resolveJogoaux (Jogo ((x:xs):ys)) (Jogador (c1,c2) Este True)
-    | direc == Oeste && caixa == False = resolveJogoaux (Jogo ((x:xs):ys)) (Jogador (c1,c2) Oeste False)
-    | direc == Oeste && caixa == True = resolveJogoaux (Jogo ((x:xs):ys)) (Jogador (c1,c2) Oeste True)
-    | otherwise = error "Incorreta identificação do Jogo"
-
--}
+import Tarefa4_2021li1g021
+import Data.List
 
 
+-- | Função: O seu objetivo é tentar resover o jogo através dos 4 movimentos possíveis
+--
+-- == Código
+-- @
+-- resolveJogo :: Int -> Jogo ->Maybe [Movimento]
+-- resolveJogo i jogo = let possivelmovimento = filter chegouporta (analisa i jogo) in if possivelmovimento == [] then Nothing else Just (criarmovimento (head (sortOn length possivelmovimento))) 
+-- @
+resolveJogo ::
+    -- | Recebe um Valo Máximo de Movimentos permitidos  
+    Int ->
+    -- | Recebe o Jogo para o qual vai resolver 
+    Jogo ->
+    -- | Devolve uma possibilidade de movimentos 
+    Maybe [Movimento]
+resolveJogo i jogo = let possivelmovimento = filter chegouporta (analisa i jogo) in if possivelmovimento == [] then Nothing else Just (criarmovimento (head (sortOn length possivelmovimento))) 
 
-movimento :: Jogo -> Maybe [Movimento]
-movimento (Jogo [] _) = Just []
-movimento (Jogo map (Jogador (xj,yj) direc caixa))
-    | checkchao (xj, (yj+1)) (xp, (yp+1)) (desconstroiMapa map) == True = contarpassos (xj, yj+1) (xp,yp+1)
-    | otherwise = error "Incompleto"
-    where (xp,yp) = (porta (desconstroiMapa map)) 
+-- | Função : Recebe uma lista de Jogo e converte para uma lista de Movimento
+--
+-- == Código
+-- @
+-- movimento ::[Jogo] ->[Movimento]
+-- movimento [] = []
+-- movimento [_] = []
+-- movimento (x:y:t) = movimentoaux x y : movimento (y:t) 
+--     where
+--         movimentoaux :: Jogo -> Jogo -> Movimento
+--         movimentoaux (Jogo _ (Jogador (_,y1) _ ca0)) (Jogo _ (Jogador (_,y2) dir ca1))
+--             | ca0 /= ca1 = InterageCaixa
+--             | y1 > y2 = Trepar
+--             | dir == Este = AndarDireita
+--             | dir == Oeste = AndarEsquerda
+--             | otherwise = error "Algum tipo de dado está incorreto"
+-- @
 
-
-checkchao :: Coordenadas -> Coordenadas -> [(Peca,Coordenadas)] -> Bool
-checkchao (xj,yj) (xp,yp) ((pe,(x,y)):xs) 
-    | xj > xp = checkchaoaux 0 (xj,yj) (xp,yp) ((pe,(x,y)):xs)
-    | xj < xp = checkchaoaux 1 (xj, yj) (xp, yp) ((pe, (x,y)):xs)
-    | otherwise = True
+movimento ::
+-- | Recebe uma Lista de Jogo
+    [Jogo] ->
+-- | Devolve uma lista de Movimentos
+    [Movimento]
+movimento [] = []
+movimento [_] = []
+movimento (x:y:t) = movimentoaux x y : movimento (y:t) 
     where
-        checkchaoaux :: Int -> Coordenadas -> Coordenadas -> [(Peca,Coordenadas)] -> Bool
-        checkchaoaux v cj cp [] = False
-        checkchaoaux v (xbj,ybj) (xp,yp) l
-            | v == 0 && xbj /= xp = elemaux (Bloco, (xbj-1,ybj)) l && checkchaoaux 0 (xbj-1,ybj) (xp,yp) l
-            | v == 1 && xbj /= xp = elemaux (Bloco, (xbj+1,ybj)) l && checkchaoaux 1 (xbj+1,ybj) (xp,yp) l
-            | v == 0 && xbj == xp || v == 1 && xbj == xp = True
+        movimentoaux ::
+            -- | Recebe um tipo Jogo
+            Jogo ->
+            -- | Rece outro tipo Jogo
+            Jogo ->
+            -- | Analisa a diferença existente e converte para Movimento
+            Movimento
+        movimentoaux (Jogo _ (Jogador (_,y1) _ ca0)) (Jogo _ (Jogador (_,y2) dir ca1))
+            | ca0 /= ca1 = InterageCaixa
+            | y1 > y2 = Trepar
+            | dir == Este = AndarDireita
+            | dir == Oeste = AndarEsquerda
+            | otherwise = error "Algum tipo de dado está incorreto"
 
 
-        elemaux :: (Peca, Coordenadas) -> [(Peca, Coordenadas)] -> Bool
-        elemaux p [] = False
-        elemaux (p,(xbj,ybj)) ((pe,(x1,y1)):xs)
-            | p == pe && xbj == x1 && ybj <= y1 = True
-            | otherwise = elemaux (p, (xbj,ybj)) xs
-
-
-porta :: [(Peca,Coordenadas)] -> Coordenadas
-porta [] = error "Não existe Porta"
-porta ((pe,(x1,y1)):xs)
-    | pe == Porta = (x1,y1)
-    | otherwise = porta xs
-
-
-contarpassos :: Coordenadas -> Coordenadas -> Maybe [Movimento]
-contarpassos (xj,yj) (xp,yp)
-    | xj < xp = andardir (xp-xj)
-    | xj > xp = andaresq (xp+xj-1)
-    | xj == xj = Just []
+-- | Função: Esta função limita-se a determinar se o Jogador terminou nas mesmas coordenadas das coordenadas da Porta
+--
+-- == Código
+-- @
+-- porta :: [Jogo] -> Bool
+-- porta jogo = (mapa !! yj) !! xj == Porta 
+--     where
+--         Jogo mapa (Jogador (xj,yj) _ _) = last jogo
+-- @
+porta ::
+    -- | Recebe uma Lista de Jogos 
+    [Jogo] -> 
+    -- | Retorna True no caso de o Jogador terminar nas mesmas coordenadas da Porta
+    Bool
+porta jogo = (mapa !! yj) !! xj == Porta 
     where
-        andaresq :: Int -> Maybe [Movimento]
-        andaresq 0 = Just []
-        andaresq n
-            | n /= 0 = Just (AndarEsquerda : andaresqaux (n-1))
-         where 
-                andaresqaux :: Int -> [Movimento]
-                andaresqaux 0 = []
-                andaresqaux n
-                    | n /= 0 = (AndarEsquerda : andaresqaux (n-1))
+        Jogo mapa (Jogador (xj,yj) _ _) = last jogo
 
-        andardir :: Int -> Maybe [Movimento]
-        andardir 0 = Just []
-        andardir n
-            | n /= 0 = Just (AndarDireita :  andardiraux (n-1))
-            where 
-                andardiraux :: Int -> [Movimento]
-                andardiraux 0 = []
-                andardiraux n
-                    | n /= 0 = (AndarDireita : andardiraux (n-1))
+-- | Função: Calcula todas as possibilidades de Jogo permitidas através do Jogo inicial
+--
+-- == Código
+-- @
+-- analisa :: Int ->Jogo -> [[Jogo]]
+-- analisa i jogo = if i <= 0 then [[jogo]] else [jogo] : map (jogo :) (concatMap (analisa (i - 1)) (criajogos jogo))
+-- @
+analisa ::
+    -- | Recebe o valor máximo de possibilidades de Movimento
+    Int ->
+    -- | Recebe um tipo Jogo (Jogos originados a partir do Jogo inicial) 
+    Jogo -> 
+    -- | Devolve uma Lista com todos os Jogos possíveis
+    [[Jogo]]
+analisa i jogo = if i <= 0 then [[jogo]] else [jogo] : map (jogo :) (concatMap (analisa (i - 1)) (criajogos jogo))
+
+-- | Função: Responsável por criar uma lista do tipo Jogo resultante da combinação das 4 movimentações permitidas no jogo
+--
+-- == Código
+-- @
+-- criajogos:: Jogo -> [Jogo]
+-- criajogos = criajogosaux [AndarEsquerda, AndarDireita, Trepar, InterageCaixa] 
+--     where
+--     criajogosaux :: [Movimento] -> Jogo -> [Jogo]
+--     criajogosaux [] _ = []
+--     criajogosaux (h:t) jogo0 = let jogo1 = moveJogador jogo0 h in if jogo1 /= jogo0 then jogo1 : criajogosaux t jogo0 else criajogosaux t jogo0
+-- @
+criajogos ::
+    -- | Recebe um tipo Jogo inicial
+    Jogo ->
+    -- | Devolve uma lista de Jogos resultante das possibilidades de movimentação
+    [Jogo]
+criajogos = criajogosaux [AndarEsquerda, AndarDireita, Trepar, InterageCaixa] 
+    where
+    criajogosaux :: [Movimento] -> Jogo -> [Jogo]
+    criajogosaux [] _ = []
+    criajogosaux (h:t) jogo0 = let jogo1 = moveJogador jogo0 h in if jogo1 /= jogo0 then jogo1 : criajogosaux t jogo0 else criajogosaux t jogo0
